bdf2dc363a8dcf27fd74170316ec3a1d207e4d5b
HerkusABG <herkus.savickas@gmail.com>
2024-10-23 15:47:14 +0200

    Additional work regarding card/hand script

---
+++ Quest-for-eternity/Assets/CardScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Quest-for-eternity/Assets/HandScript.cs
+++ Quest-for-eternity/Assets/HandScript.cs
@@ -6,18 +6,37 @@ public class HandScript : MonoBehaviour
     GameObject baseCard;
 
     [SerializeField]
+    Transform cardSpawnLocator;
+
+    [SerializeField]
     int cardCount;
     void Start()
     {
+        Debug.Log("1");
+        Vector3 cardPlacementVector = new Vector3(1,0,0);
         for(int i = 0; i < cardCount; i++)
         {
-           // Instantiate(baseCard, new Vector3(0,0,0), );
+            GameObject cardClone = Instantiate(baseCard, cardSpawnLocator.position + cardPlacementVector, Quaternion.identity);
+            cardPlacementVector += new Vector3(2, 0, 0);
+            cardClone.SetActive(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //  Physics.Raycast(ray, out RaycastHit hitInfo)
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                
+                //Debug.Log(hit.transform.name);
+              //  Debug.Log("hit");
+            }
+        }
+       
     }
 }
