8ff3b6736ac851c1d54e8c8c484699d0b126de2f
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2024-11-11 20:27:59 +0100

    Authority solve

--- Quest-for-eternity/Assets/Benjamin/PlayerScript.cs
+++ Quest-for-eternity/Assets/Benjamin/PlayerScript.cs
@@ -6,6 +6,19 @@ public class PlayerScript : NetworkBehaviour
 {
     public GameObject EndTurnButton;
 
+    public void EndTurn()
+    {
+        if (isClient)
+        {
+            CmdEndTurn();
+        }
+
+        else if (isServer)
+        {
+            TurnManagerScript.Instance.EndTurn();
+        }
+    }
+    
     [Command]
     public void CmdEndTurn()
     {
