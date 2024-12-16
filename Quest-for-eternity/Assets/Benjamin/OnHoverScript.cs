a4c6ec6c494c8fb156c30f4e67a161409e8194d1
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2024-12-16 10:03:59 +0100

    Full Card Inspection Vol1

--- Quest-for-eternity/Assets/Benjamin/OnHoverScript.cs
+++ Quest-for-eternity/Assets/Benjamin/OnHoverScript.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class OnHoverScript : MonoBehaviour
 {
     private Vector3 initialScale;
+    private Vector3 initialPosition;
+    private Vector3 moveVector = new Vector3(0.0f, 2.0f, 1.0f);
     public GameObject description;
     MeshRenderer myMeshRenderer;
     private void Awake()
     {
         initialScale = transform.root.transform.localScale;
+        initialPosition = transform.root.transform.localPosition;
         myMeshRenderer = description.GetComponent<MeshRenderer>();
         myMeshRenderer.enabled = false;
     }
@@ -25,8 +28,14 @@ public class OnHoverScript : MonoBehaviour
     private void IncreasScale(bool status)
     {
         Vector3 finalScale = initialScale;
-        if (status) {finalScale = initialScale * 2f;}
+        Vector3 finalPosition = initialPosition;
+        if (status) 
+        {
+            finalScale = initialScale * 2f;
+            finalPosition = initialPosition + moveVector;
+        }
         transform.root.transform.localScale = finalScale;
+        transform.root.transform.position = finalPosition;
         myMeshRenderer.enabled = status;
         //description.SetActive(status);
     }
