20e3f8d5bad05ce66baf83fad89f1e2288fd85c4
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2025-01-10 14:47:08 +0100

    Enemy Test 16

--- Quest-for-eternity/Assets/Benjamin/Test.cs
+++ Quest-for-eternity/Assets/Benjamin/Test.cs
@@ -4,17 +4,18 @@ using Mirror;
 public class Test : NetworkBehaviour
 {
     [SyncVar]
-    public int Health = 10;
+    public int Health;
 
     public static Test instance;
 
     private void Awake()
     {
         if(instance == null) { instance = this;}
+        Health = 10;
     }
 
     public void SubtractHealth()
     {
-        Health = -1;
+        Health -= 1;
     }
 }
