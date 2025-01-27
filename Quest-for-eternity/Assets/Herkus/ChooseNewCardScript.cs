c94d7fd103082ca143a6cac198c560b37668d1ec
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2025-01-27 22:52:14 +0100

    Trying to fix player turns 33

--- Quest-for-eternity/Assets/Herkus/ChooseNewCardScript.cs
+++ Quest-for-eternity/Assets/Herkus/ChooseNewCardScript.cs
@@ -64,17 +64,14 @@ public class ChooseNewCardScript : MonoBehaviour
                     if (displayCardCount <= 0)
                     {
                         Debug.Log("called inside");
-                        if(!playerScriptAccess.isHost)
-                        {
-                            Debug.Log("called super inside");
 
-                            //RefereeScript.instance.playerList[0].transform.root.GetComponentInChildren<ChooseNewCardScript>().DisplayCardsHidden();
-                            RefereeScript.instance.playerList[0].DisplayCardsCallNest();
-                        }
-                        else
-                        {
-                            DisplayCardsHidden();
-                        }
+                        Debug.Log("called super inside");
+
+                        //RefereeScript.instance.playerList[0].transform.root.GetComponentInChildren<ChooseNewCardScript>().DisplayCardsHidden();
+
+
+                        DisplayCardsHidden();
+
                         
                     }
                     break;
