2dba2da51df7ca943648fe94e464a985c3e8bd43
HerkusABG <herkus.savickas@gmail.com>
2025-01-28 18:28:57 +0100

    Targeting Part 3

--- Quest-for-eternity/Assets/Herkus/RefereeScript.cs
+++ Quest-for-eternity/Assets/Herkus/RefereeScript.cs
@@ -127,7 +127,7 @@ public class RefereeScript : NetworkBehaviour
         canTransferTurnToPlayer = true;
         TurnScript.restartGameEvent += RefereeReset;
         //ennemyGeneratorAccess.RandomNumber(1);
-        ennemyGeneratorAccess.GenerateEnemies(2);
+        ennemyGeneratorAccess.GenerateEnemies(1);
         //newWaveEvent += RandomizeChooseCardsSetUp;
 
         //enemyList.Add(targetEnemy);
