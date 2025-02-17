11e374ece6d692a152706abdae241048d4748683
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2025-02-17 08:08:11 +0100

    Test 4

--- Quest-for-eternity/Assets/Benjamin/PlayerObjectController.cs
+++ Quest-for-eternity/Assets/Benjamin/PlayerObjectController.cs
@@ -96,7 +96,10 @@ public class PlayerObjectController : NetworkBehaviour
         {
             Manager.GamePlayers.Remove(this);
         }
-        LobbyController.Instance.UpdatePlayerList();
+        if (LobbyController.Instance != null)
+        {
+            LobbyController.Instance.UpdatePlayerList();
+        }
     }
 
     [Command]
