efa5aeb99ca53f64faf898f0bca7023898c1c701
BenjaminEisele <167297628+BenjaminEisele@users.noreply.github.com>
2025-02-18 16:32:32 +0100

    ´Merge

--- Quest-for-eternity/Assets/Cards/Scripts/HeallByDamageEffect.cs
+++ Quest-for-eternity/Assets/Cards/Scripts/HeallByDamageEffect.cs
@@ -6,20 +6,11 @@ public class AoeEffect : EffectTemplate
 {
     public override void UseEffect<T>(int targetId, float effectValue, T value)
     {
-        //var myVar = value;
         GameObject inputGameobject = value as GameObject;
-        
-        if(inputGameobject.GetComponent<SceneObjectDatabase>() != null)
+        if (inputGameobject.GetComponent<SceneObjectDatabase>() != null)
         {
-            SceneObjectDatabase sceneObjectDatabaseAccess = inputGameobject.GetComponent<SceneObjectDatabase>();
-           // sceneObjectDatabaseAccess.playerStatReference.playerHealth += (int)effectValue;
-            sceneObjectDatabaseAccess.playerStatReference.ChangePlayerHealth((int)effectValue);
-            //Debug.Log(sceneObjectDatabaseAccess.playerStatReference.playerHealth);
+            inputGameobject.GetComponent<SceneObjectDatabase>().playerScriptAccess.shouldHealByDamageAmount = true;
         }
-        else
-        {
-            Debug.Log("nepavyko");
-        }
-        //Debug.Log($"I healed {targetId}! The value was: {value}");
+
     }
 }
