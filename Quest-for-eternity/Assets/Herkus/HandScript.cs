56876e22fc68616f434ab8b9c800dbccb95333f4
HerkusABG <herkus.savickas@gmail.com>
2025-02-25 17:17:52 +0100

    WfoH debugging part 2

--- Quest-for-eternity/Assets/Herkus/HandScript.cs
+++ Quest-for-eternity/Assets/Herkus/HandScript.cs
@@ -733,6 +733,7 @@ public class HandScript : MonoBehaviour
         cardCount = 0;
         cardDebt = 0;
         cardQueIndex = 0;
+        utlCardsPlayedForOtherPlayer = 0;
         CardInstantiation();
         RebuildCardListLite();
     }
