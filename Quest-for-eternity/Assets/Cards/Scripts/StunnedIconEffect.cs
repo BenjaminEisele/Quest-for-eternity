b50f97072eef7c7decbe130473ce93c4b9be2ddd
CTAres <ledowe@stud.mediadesign.de>
2025-02-25 14:27:55 +0100

    Annoying Sprites Icon added

--- Quest-for-eternity/Assets/Cards/Scripts/StunnedIconEffect.cs
+++ Quest-for-eternity/Assets/Cards/Scripts/StunnedIconEffect.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class StunnedIconEffect
+
+[CreateAssetMenu(fileName = "StunnedIconEffect", menuName = "Effect/StunnedIconEffect")]
+[System.Serializable]
+public class StunnedIconEffect : EffectTemplate
 {
-    
+    public override void UseEffect<T>(int targetId, float effectValue, T value)
+    {
+        GameObject inputGameobject = value as GameObject;
+        if (inputGameobject.GetComponent<SceneObjectDatabase>() != null)
+        {
+            inputGameobject.GetComponent<SceneObjectDatabase>().handScriptReference.SendCardsOver(null, inputGameobject.GetComponent<SceneObjectDatabase>().databasePlayerAccess.cardList.Count - 9);
+        }
+    }
 }
