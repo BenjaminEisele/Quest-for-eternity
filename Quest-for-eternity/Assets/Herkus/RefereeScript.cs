b79476ddb0935b5cc25581327072943dd4ae6570
HerkusABG <herkus.savickas@gmail.com>
2025-02-25 14:15:55 +0100

    Debugging turn transfer

--- Quest-for-eternity/Assets/Herkus/RefereeScript.cs
+++ Quest-for-eternity/Assets/Herkus/RefereeScript.cs
@@ -672,6 +672,7 @@ public class RefereeScript : NetworkBehaviour
         if (playerList[targetPlayerId].transform.root.GetComponentInChildren<PlayerStatScript>().TakeDamageAndCheckIfDead(inputDamage, inputType, voiceReference))
         {
             TurnScript.instance.ShouldStartPlayerTurn(false);
+            Debug.Log("set to false");
             playerList[targetPlayerId].isPlayerAlive = false;
             if (AreAllPlayersDead())
             {
@@ -680,6 +681,8 @@ public class RefereeScript : NetworkBehaviour
         }
         else
         {
+            Debug.Log("set to true");
+
             playerList[targetPlayerId].isPlayerAlive = true;
         }
     }
